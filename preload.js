02efa1827064eb1353ca072db20245d36087f2b6
Layne Hymel <laynehy@gmail.com>
2025-05-31 04:53:48 -0500

    remove preload

--- preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-window.addEventListener('contextmenu', (e) => {
-  const selection = window.getSelection().toString();
-  if (selection.trim().length > 0) {
-    e.preventDefault();
-    ipcRenderer.send('show-context-menu', selection);
-  }
-}, false);
